80a4a061771d13d49b0768c1af276df56cfa136f
Abid Hussain <abii315hussain@gmail.com>
2021-08-06 21:47:22 +0500

    agora token code added

--- app.js
+++ app.js
@@ -20,6 +20,26 @@ app.get('/',(req,res)=>{
     res.send("api is working");
 })
 
+app.post('/agora-access-token', (req, res) => {
+ 
+    const appID = 'a6b42ead96b54e2a897239f272892a6e';
+    const appCertificate = '2eec4082e6dd47d38893a1b7e0ce9797';
+    const channelName = req.body.channelName;
+    const uid = req.body.uid;
+    const account = req.body.account;
+    const role = RtcRole.PUBLISHER;
+    
+    const expirationTimeInSeconds = 3600
+    
+    const currentTimestamp = Math.floor(Date.now() / 1000)
+    
+    const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
+    
+    const tokenA = RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channelName, uid, role, privilegeExpiredTs);
+    
+    res.status(200).json({"status":200,"token":tokenA});
+    });
+
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
